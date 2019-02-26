670ad17a3305ab4811eb3fb185ff8a38480555e6
wanching_97 <wanching_97@hotmail.com>
2019-02-26 15:30:51 +0800

    added custom validation for isbn

---
+++ app/Http/Requests/SaveBookRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\IsIsbn;
+
+class SaveBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // 'isbn' => ['required', 'unique:books', 'regex:/^(97(8|9))?\d{9}(\d|X)$/'],
+            'isbn' => ['required', 'unique:books', new IsIsbn],
+            'title' => 'max:200',
+            'year' => 'required'
+        ];
+    }
+
+    // if title == bad, throw exception
+    public function withValidator($validator) {
+        $validator->after(function ($validator) { 
+            if ($this->input('title') == "bad") {
+                $validator->errors()->add('field', 'Something is wrong with this field!'); 
+            }
+    }); }
+}
