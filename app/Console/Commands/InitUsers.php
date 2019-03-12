c771a59210379d431d88d007dd6f90f3658f7f09
wanching_97 <wanching_97@hotmail.com>
2019-03-12 15:29:07 +0800

    added command to create default superuser

--- app/Console/Commands/InitUsers.php
+++ app/Console/Commands/InitUsers.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\User;
 use Illuminate\Console\Command;
 
 class InitUsers extends Command
@@ -11,16 +12,34 @@ class InitUsers extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'init:users';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Initialize default users';
 
-    /**
+    private $users_data = [
+        [
+            'name' => 'Administrator',
+            'email' => 'admin@mylib.info',
+            'password' => 'adminpass'
+        ],
+        [
+            'name' => 'User One',
+            'email' => 'user1@mylib.info',
+            'password' => 'user1pass'
+        ],
+        [
+            'name' => 'User Two',
+            'email' => 'user2@mylib.info', 
+            'password' => 'user2pass',
+        ],
+    ];
+
+    /** 
      * Create a new command instance.
      *
      * @return void
@@ -37,6 +56,14 @@ class InitUsers extends Command
      */
     public function handle()
     {
-        //
+        foreach($this->users_data as $user_data) {
+            $user = new User;
+            $user->name = $user_data['name'];
+            $user->email = $user_data['email'];
+            $user->password = bcrypt($user_data['password']);
+            $user->save();
+
+            echo "User $user->email created successfully\n";
+        }
     }
 }
